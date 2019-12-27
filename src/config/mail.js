50598379eca584ba2d61a51762647136deb50b2c
Daniel <daniel_sousa.p@outlook.com>
2019-12-27 18:17:53 -0300

    Configurando o nodemailer e o template de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: 'd985a615ac5188',
+        pass: 'b0d96adaa22bc7',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
