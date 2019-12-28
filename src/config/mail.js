325262b2b664fda9fc5a648fd6888bae455f6933
Daniel <daniel_sousa.p@outlook.com>
2019-12-28 13:50:47 -0300

    Váriaveis ambiente da aplicação

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-    host: 'smtp.mailtrap.io',
-    port: 2525,
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_port,
     secure: false,
     auth: {
-        user: 'd985a615ac5188',
-        pass: 'b0d96adaa22bc7',
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
     },
     default: {
         from: 'Equipe GoBarber <noreply@gobarber.com>',
